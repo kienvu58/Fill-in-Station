b6741cfba8c1c759c31f207a7995f0cc7c52fc4f
Kien Vu <vtk5995@gmail.com>
2016-03-26 19:29:46 +0700

    add advanced heuristic function

--- fill_in_station.py
+++ fill_in_station.py
@@ -76,22 +76,26 @@ class FillinStationProblem:
         """
         return len(state) == 9 and self.check_contraints(state)
 
-    def get_successors(self, state):
+    def get_successors(self, state, heuristic_fn):
         """
             state: current state
         Returns priority queue of next states
         """
         successors = PriorityQueue()
-        next_var_index = len(state)
+        next_index = len(state)
         curr_domain = self.domain[:]
         for key in state.keys():
             curr_domain.remove(state[key])
         for value in curr_domain:
-            heuristic = self.get_heuristic(state, next_var_index, value)
+            heuristic = heuristic_fn(
+                self.bigram_freq, self.variables, state, next_index, value)
+
             if heuristic == 0:  # bigram never happens
                 continue
+
             next_state = state.copy()
-            next_state.update({self.variables[next_var_index]: value})
+            next_state.update({self.variables[next_index]: value})
+
             if self.check_contraints(next_state):
                 successors.push(next_state, heuristic)
 
@@ -110,53 +114,93 @@ class FillinStationProblem:
 
         return True
 
-    def get_heuristic(self, state, next_var_index, next_value):
-        """
-            state: current state
-            next_var_index: next variable position in matrix
-            next_value: value of next variable need to calculate heuristic
-        Returns heuristic value for assigning next variable from current
-        state.
-        """
-        heuristic = 1
-        if next_var_index in (0, 3, 6):
-            heuristic *= self.bigram_freq[('$', next_value)]
-        else:
-            prev_value = state[self.variables[next_var_index - 1]]
-            heuristic *= self.bigram_freq[(prev_value, next_value)]
 
-        return - heuristic
-
-
-def backtracking_search(problem, trace_fn=None):
+def normal_heuristic(bigram_freq, variables, state, next_index, next_value):
     """
-        trace_fn: name of trace file, None means no trace
+        state: current state
+        next_index: next variable position in matrix
+        next_value: value of next variable need to calculate heuristic
+    Returns heuristic value for assigning next variable from current
+    state.
+    This is a simple heuristic function.
+    """
+    heuristic = 1
+    if next_index in (0, 3, 6):
+        heuristic *= bigram_freq[('$', next_value)]
+    else:
+        prev_value = state[variables[next_index - 1]]
+        heuristic *= bigram_freq[(prev_value, next_value)]
+
+    return - heuristic
+
+
+def advanced_heuristic(bigram_freq, variables, state, next_index, next_value):
+    """
+        state: current state
+        next_index: next variable position in matrix
+        next_value: value of next variable need to calculate heuristic
+    Returns heuristic value for assigning next variable from current
+    state.
+    This is an advanced heuristic function.
+    """
+    heuristic = 1
+    # horizontal
+    if next_index in (0, 3, 6):
+        heuristic *= bigram_freq[('$', next_value)]
+    else:
+        prev_value = state[variables[next_index - 1]]
+        heuristic *= bigram_freq[(prev_value, next_value)]
+
+    # vertical
+    if next_index > 2:
+        prev_value = state[variables[next_index - 3]]
+        heuristic *= bigram_freq[prev_value, next_value]
+
+    # main diagonal
+    if next_index in (4, 8):
+        prev_value = state[variables[next_index - 4]]
+        heuristic *= bigram_freq[prev_value, next_value]
+
+    # anti diagonal
+    if next_index in (4, 6):
+        prev_value = state[variables[next_index - 2]]
+        heuristic *= bigram_freq[prev_value, next_value]
+
+    return - heuristic
+
+
+def backtracking_search(problem, heuristic_fn=advanced_heuristic, log=None):
+    """
+        log: name of trace file, None means no trace
     Returns a solution for fill-in station problem.
     Use heuristic to choose which value goes first.
     """
-    def recursive_backtracking(state, problem, trace_fn):
+    def recursive_backtracking(state, problem, heuristic_fn, log):
+        problem.count += 1
         if problem.is_goal_state(state):
             return state
 
-        successors = problem.get_successors(state)
+        successors = problem.get_successors(state, heuristic_fn)
         children = successors.heap[:]
+
+        if log is not None:
+            print_trace(log, state, children, problem.count)
 
         while not successors.isEmpty():
             next_state = successors.pop()
-            result = recursive_backtracking(next_state, problem, trace_fn)
+            result = recursive_backtracking(
+                next_state, problem, heuristic_fn, log)
+
             if result is not None:
                 return result
-        problem.count += 1
-        if trace_fn is not None:
-            print_trace(trace_fn, state, children, problem.count)
 
         return None
 
-    def print_trace(trace_fn, parent, children, n_nodes):
-        f = open(trace_fn, 'a')
-        f.write(str(n_nodes))
+    def print_trace(log, parent, children, n_nodes):
+        f = open(log, 'a')
+        f.write(str(n_nodes) + '\n')
 
-    return recursive_backtracking({}, problem, trace_fn)
+    return recursive_backtracking({}, problem, heuristic_fn, log)
 
 
 def get_dictionary(filename):
@@ -210,21 +254,6 @@ if __name__ == "__main__":
     dict = get_dictionary('3_letters_dictionary')
     bigram_freq = get_bigram_freq('bigram_frequence_list')
     problem = FillinStationProblem(dom1, dict, bigram_freq)
-    right_assignment = {'11': 'S', '12': 'O', '13': 'P',
-                        '21': 'E', '22': 'A', '23': 'R',
-                        '31': 'W', '32': 'R', '33': 'Y'}
-    incomplete_assignment = {'11': 'S', '12': 'O', '13': 'P',
-                             '21': 'E', '22': 'A', '23': 'R',
-                             '31': 'W', '32': 'R'}
-    wrong_assignment = {'11': 'S', '12': 'O', '13': 'R',
-                        '21': 'E', '22': 'A', '23': 'P',
-                        '31': 'W', '32': 'R', '33': 'Y'}
-    wrong_incomplete = {'32': 'R', '31': 'W', '33': 'Y',
-                        '11': 'S', '12': 'O', '13': 'P'}
-    # print problem.check_contraints(right_assignment)
-    # print problem.check_contraints(incomplete_assignment)
-    # print problem.check_contraints(wrong_assignment)
-    # print problem.check_contraints(wrong_incomplete)
 
     result = backtracking_search(problem)
     if result is not None:
